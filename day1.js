9e64ed652e090f10247ea6e32b0cf980e4e8d371
黄施嘉 <huangshijia1@huangshijiadeMacBook-Pro.local>
2024-04-23 21:24:37 +1000

    day1 completed

---
+++ day1.js
@@ -0,0 +1,21 @@
+const divs = document.querySelectorAll('.container div');
+console.log(divs);
+
+function changeName(element){
+    divs.forEach(div => {
+        div.className = "img";
+        const h3 = div.querySelector('h3');
+        h3.style.display = "none";
+    });
+    element.className = "imgActive";
+};
+
+divs.forEach(element => {
+        element.onclick = function(){
+            changeName(element);
+            const h3 = element.querySelector('h3');
+            setTimeout(function(){
+                h3.style.display = "block";
+            }, 1500);
+        }
+    });
